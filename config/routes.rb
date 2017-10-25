fc7e5065a57c23581817b367c1c45a195997cb9a
Duy DANG <tao.zen.duy@gmail.com>
2017-10-25 23:30:59 +0200

    routes 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "gossips#idex"
+  root "gossips#index"
   resources :gossips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
